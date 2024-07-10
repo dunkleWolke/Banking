b6cdda67d5a11a9ef14531766eaf0d0fc12177d1
dunkleWolke <94968223+Zveruha@users.noreply.github.com>
2024-07-11 01:40:53 +0300

    add item to main screen

---
+++ app/src/main/java/com/example/banking/screens/main/presentation/ui/RecentTransactionsBlock.kt
@@ -0,0 +1,18 @@
+package com.example.banking.screens.main.presentation.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.example.banking.database.model.Transaction
+import com.example.banking.screens.transactions.presentation.ui.TransactionItem
+
+@Composable
+fun RecentTransactionsBlock(transactions: List<Transaction>,  onTransactionClick: (Transaction) -> Unit) {
+    val sortedTransactions = transactions.sortedByDescending { it.date }
+    val recentTransactions = sortedTransactions.take(5)
+
+    Column {
+       recentTransactions.forEach { transaction ->
+            TransactionItem(transaction, onTransactionClick)
+      }
+    }
+}
